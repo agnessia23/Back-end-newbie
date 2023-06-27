5aaa710a28905fbc515b8c47f9887c5eaab09472
Agnes Glory Siahaan <125450406+agnessia23@users.noreply.github.com>
2023-06-27 10:47:06 +0700

    adding files
     This website is a submission assignment class "Learn to make back-end applications for beginners" Dicoding.

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+  await server.start();
+
+  console.log(`Server Started at ${server.info.uri}\n`);
+};
+
+init();
